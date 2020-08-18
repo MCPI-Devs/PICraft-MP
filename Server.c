02eee693e27643492dfa951c5279e2129131fc98
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-08-19 01:53:32 +0300

    Update Server.c

--- Server.c
+++ Server.c
@@ -1,9 +1,10 @@
-//   ____ ___ ____            __ _        __  __ ____  
-//  |  _ \_ _/ ___|_ __ __ _ / _| |_     |  \/  |  _ \ 
-//  | |_) | | |   | '__/ _` | |_| __|____| |\/| | |_) |
-//  |  __/| | |___| | | (_| |  _| ||_____| |  | |  __/ 
-//  |_|  |___\____|_|  \__,_|_|  \__|    |_|  |_|_|    
-//
-// Licensed under the Apache License, Version 2.0 (the "License")
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+/*   ____ ___ ____            __ _        __  __ ____  
+ *  |  _ \_ _/ ___|_ __ __ _ / _| |_     |  \/  |  _ \ 
+ *  | |_) | | |   | '__/ _` | |_| __|____| |\/| | |_) |
+ *  |  __/| | |___| | | (_| |  _| ||_____| |  | |  __/ 
+ *  |_|  |___\____|_|  \__,_|_|  \__|    |_|  |_|_|    
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ */
