eacb49bf2bf5ca115bd3f8e410049b6633fba651
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-08-19 01:52:12 +0300

    Update Server.c

--- Server.c
+++ Server.c
@@ -4,6 +4,6 @@
 //  |  __/| | |___| | | (_| |  _| ||_____| |  | |  __/ 
 //  |_|  |___\____|_|  \__,_|_|  \__|    |_|  |_|_|    
 //
-//
-//
-//
+// Licensed under the Apache License, Version 2.0 (the "License")
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
