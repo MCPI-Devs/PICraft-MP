6a5a162ba3bdf4d66013fc108e49ef58b73d43df
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-03-10 20:48:23 +0200

    Update t_format.h

--- constant/t_format.h
+++ constant/t_format.h
@@ -1,7 +1,7 @@
 #ifndef T_FORMAT_H
 #define T_FORMAT_H
 
-#define BLACK "\x1b[30m"
+#define DARK_BLACK "\x1b[30m"
 #define DARK_RED "\x1b[31m"
 #define DARK_GREEN "\x1b[32m"
 #define DARK_YELLOW "\x1b[33m"
@@ -9,19 +9,13 @@
 #define DARK_MAGENTA "\x1b[35m"
 #define DARK_CYAN "\x1b[36m"
 #define DARK_WHITE "\x1b[37m"
-#define BRIGHT_BLACK "\x1b[90m"
-#define BRIGHT_RED "\x1b[91m"
-#define BRIGHT_GREEN "\x1b[92m"
-#define BRIGHT_YELLOW "\x1b[93m"
-#define BRIGHT_BLUE "\x1b[94m"
-#define BRIGHT_MAGENTA "\x1b[95m"
-#define BRIGHT_CYAN "\x1b[96m"
-#define WHITE "\x1b[97m"
-#define BOLD "\x1b[1m"
-#define UNDERLINE "\x1b[4m"
-#define NO_UNDERLINE "\x1b[24m"
-#define REVERSE_TEXT "\x1b[7m"
-#define POSITIVE_TEXT "\x1b[27m"
-#define DEFAULT "\x1b[0m"
+#define LIGHT_BLACK "\x1b[1;30m"
+#define LIGHT_RED "\x1b[1;31m"
+#define LIGHT_GREEN "\x1b[1;32m"
+#define LIGHT_YELLOW "\x1b[1;33m"
+#define LIGHT_BLUE "\x1b[1;34m"
+#define LIGHT_MAGENTA "\x1b[1;35m"
+#define LIGHT_CYAN "\x1b[1;36m"
+#define LIGHT_WHITE "\x1b[1;37m"
 
 #endif // T_FORMAT_H
