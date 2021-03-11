b60be2f61abc4daebae8dedafc2b07198925399c
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-03-11 23:57:32 +0200

    Update buffer.h

--- utils/buffer.h
+++ utils/buffer.h
@@ -15,9 +15,9 @@ unsigned int byte_array_size(byte_array_t _byte_array);
 
 void reset_buffer(buffer_t *_buffer);
 
-void write_buffer(buffer_t *_buffer, char *data);
+void write_buffer(buffer_t *_buffer, byte_array_t _byte_array);
 
-char* read_buffer(buffer_t *_buffer, unsigned int length);
+byte_array_t read_buffer(buffer_t *_buffer, unsigned int length);
 
 unsigned int end_of_buffer(buffer_t *_buffer);
 
