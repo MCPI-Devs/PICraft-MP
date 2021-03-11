6a8eea11c2e02b8f8d039f8ac020532b95b2c5f5
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-03-11 23:27:13 +0200

    Create byte_array.c

---
+++ utils/byte_array.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <utils/byte_array.h>
+
+unsigned int byte_array_size(byte_array_t _byte_array) {
+    return (sizeof(_byte_array.v) / sizeof(_byte_array.v[o]))
+}
