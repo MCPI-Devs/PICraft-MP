f03783d1d6daf524adc48320a850b33f8c0a42f3
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-03-11 23:47:16 +0200

    Update byte_array.c

--- utils/byte_array.c
+++ utils/byte_array.c
@@ -4,5 +4,5 @@
 #include <utils/byte_array.h>
 
 unsigned int byte_array_size(byte_array_t _byte_array) {
-    return (sizeof(_byte_array.v) / sizeof(_byte_array.v[o]))
+    return (sizeof(_byte_array.v) / sizeof(_byte_array.v[0]));
 }
