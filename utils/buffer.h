51035a850d5e39637923b1f91c2673638f72307d
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-03-12 09:57:58 +0200

    Create buffer.h

---
+++ utils/buffer.h
@@ -0,0 +1,15 @@
+#ifndef BUFFER_H
+#define BUFFER_H
+
+typedef struct {
+    char *data;
+    int pos;
+} buffer_t;
+
+void append_buffer(buffer_t *buffer, char* data);
+
+char *pop_buffer(buffer_t *buffer, unsigned int length);
+
+buffer_t new_buffer();
+
+#endif
