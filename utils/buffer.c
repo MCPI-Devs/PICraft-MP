1c1c3ed453cc987415958a2cbaf12c35ebf2bd6f
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-03-13 00:44:13 +0200

    Update buffer.c

--- utils/buffer.c
+++ utils/buffer.c
@@ -7,70 +7,24 @@
 
 buffer_t new_buffer() {
     buffer_t buffer;
-    buffer.data = "";
-    buffer.pos = 0;
+    buffer.r_pos = 0;
+    buffer.w_pos = 0;
     return buffer;
 }
 
-char *pop_buffer(buffer_t *buffer, unsigned int length) {
-    char* value = substr(buffer->data, buffer->pos, length);
-    buffer->pos += length;
+char *read_one(buffer_t *buffer) {
+    char value = buffer.data[buffer->r_pos]
+    ++buffer->r_pos;
     return value;
 }
 
-void append_buffer(buffer_t *buffer, char* data) {
-    buffer->data = joinstr(buffer->data, data);
+void write_one(buffer_t *buffer, char one) {
+    buffer->data[buffer->w_pos] = one;
+    ++buffer->w_pos;
 }
 
 void reset_buffer(buffer_t *buffer) {
-    buffer->data = "";
-    buffer->pos = 0;
-}
-
-char read_int_8(buffer_t *buffer) {
-    char result = pop_buffer(buffer, 1)[0];
-    return result;
-}
-
-void write_int_8(buffer_t *buffer, char value) {
-    append_buffer(buffer, &value);
-}
-
-unsigned char read_u_int_8(buffer_t *buffer) {
-    unsigned char result = pop_buffer(buffer, 1)[0];
-    return result;
-}
-
-void write_u_int_8(buffer_t *buffer, unsigned char value) {
-    append_buffer(buffer, &value);
-}
-
-short read_int_16(buffer_t *buffer) {
-    char *data = pop_buffer(buffer, 2);
-    short result = data[0];
-    result |= (data[1] << 8);
-    return result;
-}
-
-void write_int_16(buffer_t *buffer, short value) {
-    char *result = malloc(3);
-    result[0] = (value & 0xff);
-    result[1] = ((value >> 8) & 0xff);
-    result[2] = 0x00;
-    append_buffer(buffer, result);
-}
-
-unsigned short read_u_int_16(buffer_t *buffer) {
-    char *data = pop_buffer(buffer, 2);
-    unsigned short result = data[0];
-    result |= (data[1] << 8);
-    return result;
-}
-
-void write_u_int_16(buffer_t *buffer, unsigned short value) {
-    char *result = malloc(3);
-    result[0] = (value & 0xff);
-    result[1] = ((value >> 8) & 0xff);
-    result[2] = 0x00;
-    append_buffer(buffer, result);
+    memset(&buffer->data, 0, sizeof(buffer->data));
+    buffer.r_pos = 0;
+    buffer.w_pos = 0;
 }
